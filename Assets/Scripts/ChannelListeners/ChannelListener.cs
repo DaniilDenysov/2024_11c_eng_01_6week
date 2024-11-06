88d41529b73446ebeb0b185694520a58868ef096
Daniil <den.danya.denysov@gmail.com>
2024-11-06 19:18:05 +0100

    Added EventChannels and EventChannelListeners
    Added test sounds, tested channels, added SFXEventListener and and MusicEventListener

---
+++ Assets/Scripts/ChannelListeners/ChannelListener.cs
@@ -0,0 +1,28 @@
+using DesignPatterns.Singleton;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ChannelListener<T> : Singleton<ChannelListener<T>>
+{
+    [SerializeField] protected EventChannel<T> eventChannel;
+
+    public override ChannelListener<T> GetInstance()
+    {
+        return this;
+    }
+
+    public override void Awake()
+    {
+        base.Awake();
+        Subscribe();
+    }
+
+    public void OnDestroy()
+    {
+        Unsubscribe();
+    }
+
+    public abstract void Subscribe();
+    public abstract void Unsubscribe();
+}
