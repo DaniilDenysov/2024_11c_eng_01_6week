07a8cb859addcbca7f3bbbc7d882c2f534e2d1a3
Oleksandr Pryimak <pryimaksasha@gmail.com>
2024-11-08 19:52:13 +0100

    Closing window animation

--- Assets/Scripts/Animations/ViewAnimationHandler.cs
+++ Assets/Scripts/Animations/ViewAnimationHandler.cs
@@ -6,21 +6,22 @@ public class ViewAnimationHandler : MonoBehaviour
     [SerializeField] private float animationDuration = 0.25f;
     [SerializeField] private float initialScale = 0.9f;
     [SerializeField] private float endScale = 1f;
-    [SerializeField] private float finalScale = 0.6f;
-    [SerializeField] private float bounceIntencity = 2f;
+    [SerializeField] private float finalScale = 0.8f;
+    [SerializeField] private float bounceIntensity = 2f;
     private void OnEnable()
     {
         transform.localScale = Vector3.one * initialScale;
 
         transform.DOScale(endScale, animationDuration)
-        .SetEase(Ease.OutBack, bounceIntencity)
+        .SetEase(Ease.OutBack, bounceIntensity)
         .From(Vector3.one * initialScale);
     }
 
-    private void OnDisable()
+    public void CloseWindow()
     {
         transform.DOScale(finalScale, animationDuration)
-            .SetEase(Ease.OutBack, bounceIntencity)
-            .From(Vector3.one * endScale);
+            .SetEase(Ease.InBack, bounceIntensity)
+            .From(Vector3.one * endScale)
+            .OnComplete(() => gameObject.transform.parent.gameObject.SetActive(false));
     }
 }
